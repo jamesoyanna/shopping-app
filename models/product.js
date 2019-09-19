a54479f42d6140be1fc8a8bcdc119ba91b922009
eshoiza <engrjayt200@gmail.com>
2019-09-20 00:54:42 +0100

    added the product model

---
+++ models/product.js
@@ -0,0 +1,14 @@
+const fs = requie('fs');
+const path = require('path');
+
+module.exports= class Product{
+    constructor(t){
+this.title = t;
+    }
+    save(){
+        products.push(this);
+    }
+    static fetchAll(){
+        return products;
+    }
+}
